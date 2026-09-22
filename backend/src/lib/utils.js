5d895e37e800eb8a02bc3ab9e6c3a63da6c71aa6
Shashank Priyadarshi <shashank.kvs2019@gmail.com>
2026-09-23 02:49:27 +0530

    Change cookie sameSite to 'none' and secure to true
    Updated cookie settings for cross-domain support.

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -14,12 +14,9 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite: "strict", // CSRF attacks
-    secure: ENV.NODE_ENV === "development" ? false : true,
+    sameSite: "none", // 👈 "strict" se badalkar "none" karna hai cross-domain ke liye
+    secure: true,     // 👈 Cross-domain cookies ke liye secure true hona zaroori hai (HTTPS)
   });
 
   return token;
 };
-
-// http://localhost
-// https://dsmakmk.com
