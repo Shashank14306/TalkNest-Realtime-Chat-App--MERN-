4371f07c8a35c47c579d12e1827215e79dbbbc18
Shashank Priyadarshi <shashank.kvs2019@gmail.com>
2026-09-23 02:32:25 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development" 
     ? "http://localhost:3000/api" 
-    : "https://talknest-realtime-chat-app-mern.onrender.com/api",
+    : "https://talk-nest-realtime-chat-app-mern.onrender.com/api",
   withCredentials: true,
 });
